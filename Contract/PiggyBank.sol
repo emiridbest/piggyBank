b6618e81d5565f9ecffb7e7f55bc57f3563e7494
emiridbest <Udogwuemiri@gmail.com>
2023-06-28 16:22:45 +0100

    Update PiggyBank.sol

--- Contract/PiggyBank.sol
+++ Contract/PiggyBank.sol
@@ -1,51 +1,107 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract PiggyBank {
-    address payable owner;
-    uint256 public depositTime;
+    struct TokenBalance {
+        uint256 celoBalance;
+        uint256 cUsdBalance;
+        uint256 depositTime;
+    }
+
+    mapping(address => TokenBalance) public balances;
     uint256 public lockDuration = 1 weeks;
-    bool public isDepositMade = false;
+    address private constant CELO_TOKEN_ADDRESS = address(0);
+    address private constant CUSD_TOKEN_ADDRESS =0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
 
-    constructor() {
-        owner = payable(msg.sender);
+    event Deposited(
+        address indexed depositor,
+        uint256 amount,
+        address indexed token
+    );
+    event Withdrawn(
+        address indexed withdrawer,
+        uint256 amount,
+        address indexed token
+    );
+
+    receive() external payable {
+        deposit(CELO_TOKEN_ADDRESS, msg.value);
     }
 
-    event Deposited(uint256 amount);
-    event Withdrawn(uint256 amount);
+    function deposit(address tokenAddress, uint256 amount) public {
+        if (tokenAddress == CELO_TOKEN_ADDRESS) {
+            require(amount > 0, "CELO deposit amount must be greater than 0");
+            TokenBalance storage celoBalance = balances[msg.sender];
+            celoBalance.celoBalance += amount;
+            celoBalance.depositTime = block.timestamp;
+            emit Deposited(msg.sender, amount, CELO_TOKEN_ADDRESS);
+        } else if (tokenAddress == CUSD_TOKEN_ADDRESS) {
+            IERC20 cUsdToken = IERC20(CUSD_TOKEN_ADDRESS);
+            require(
+                cUsdToken.transferFrom(msg.sender, address(this), amount),
+                "Transfer failed. Make sure to approve the contract to spend the cUSD tokens."
+            );
+            TokenBalance storage cUsdBalance = balances[msg.sender];
+            cUsdBalance.cUsdBalance += amount;
+            cUsdBalance.depositTime = block.timestamp;
+            emit Deposited(msg.sender, amount, CUSD_TOKEN_ADDRESS);
+        } else {
+            revert("Unsupported token");
+        }
+    }
 
-    function deposit() public payable {
+    function timeSinceDeposit(address depositor) public view returns (uint256) {
+        return block.timestamp - balances[depositor].depositTime;
+    }
+
+    function canWithdraw(address depositor) public view returns (bool) {
+        TokenBalance storage tokenBalance = balances[depositor];
+        return ((tokenBalance.celoBalance > 0 &&
+            timeSinceDeposit(depositor) >= lockDuration) ||
+            (tokenBalance.cUsdBalance > 0 &&
+                timeSinceDeposit(depositor) >= lockDuration));
+    }
+
+    function withdraw(address tokenAddress) public {
         require(
-            msg.sender == owner,
-            "Only the contract owner can deposit funds"
+            canWithdraw(msg.sender),
+            "Cannot withdraw before lock duration or no tokens deposited"
         );
-        require(
-            isDepositMade == false,
-            "Deposit is already made, wait until withdrawal"
-        );
-        depositTime = block.timestamp;
-        isDepositMade = true;
-        emit Deposited(msg.value);
+
+        TokenBalance storage tokenBalance = balances[msg.sender];
+        uint256 amount;
+
+        if (tokenAddress == CELO_TOKEN_ADDRESS) {
+            amount = tokenBalance.celoBalance;
+            tokenBalance.celoBalance = 0;
+            payable(msg.sender).transfer(amount);
+        } else if (tokenAddress == CUSD_TOKEN_ADDRESS) {
+            amount = tokenBalance.cUsdBalance;
+            tokenBalance.cUsdBalance = 0;
+            IERC20 cUsdToken = IERC20(CUSD_TOKEN_ADDRESS);
+            require(cUsdToken.transfer(msg.sender, amount), "Transfer failed");
+        } else {
+            revert("Unsupported token");
+        }
+
+        emit Withdrawn(msg.sender, amount, tokenAddress);
     }
 
-    function timeSinceDeposit() public view returns (uint256) {
-        require(isDepositMade == true, "No deposit has been made yet");
-        return block.timestamp - depositTime;
-    }
+    function getBalance(address account, address tokenAddress)
+        public
+        view
+        returns (uint256)
+    {
+        TokenBalance storage tokenBalance = balances[account];
 
-    function canWithdraw() public view returns (bool) {
-        require(isDepositMade == true, "No deposit has been made yet");
-        return timeSinceDeposit() >= lockDuration;
-    }
-
-    function withdraw() public {
-        require(
-            msg.sender == owner,
-            "Only the contract owner can withdraw funds"
-        );
-        require(canWithdraw(), "Cannot withdraw before lock duration");
-        uint256 amount = address(this).balance;
-        owner.transfer(amount);
-        isDepositMade = false;
+        if (tokenAddress == CELO_TOKEN_ADDRESS) {
+            return tokenBalance.celoBalance;
+        } else if (tokenAddress == CUSD_TOKEN_ADDRESS) {
+            return tokenBalance.cUsdBalance;
+        } else {
+            revert("Unsupported token");
+        }
     }
 }
